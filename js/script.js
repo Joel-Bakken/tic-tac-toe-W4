610c956f1aed2a03a6c4a379e074f5ff51059293
Esvethlynna Pangelinan, Joel Bakken, Laura Hamilton and Mick Sexton <laura@>
2017-06-15 13:41:27 -0700

    add click functions to remaining board squares and player auto switch upon click

--- js/script.js
+++ js/script.js
@@ -26,7 +26,15 @@ function Player(mark) {
 
 Gameboard.prototype.change = function(newSpace) {
 	this.board[newSpace.xCoord][newSpace.yCoord] = newSpace.markedBy;
-  alert(newSpace.xCoord + " " + newSpace.yCoord + " " + newSpace.markedBy);
+  //alert(newSpace.xCoord + " " + newSpace.yCoord + " " + newSpace.markedBy);
+}
+
+function switchPlayer() {
+  if (currentPlayer === playerX) {
+    currentPlayer = playerO;
+  } else if (currentPlayer === playerO) {
+    currentPlayer = playerX;
+  }
 }
 
 
@@ -46,6 +54,7 @@ $(document).ready(function() {
     gameBoard.change(newSpace);
     //output AddClass X or O or text from currentPlayer.mark within span in li
     $("#out-r0c0").text(currentPlayer.mark);
+    switchPlayer();
   });
   //click listener 2...
   $("#r0c1").click(function(event) {
@@ -53,12 +62,56 @@ $(document).ready(function() {
     var newSpace = new Space(0,1, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r0c1").text(currentPlayer.mark);
+    switchPlayer();
   });
   $("#r0c2").click(function(event) {
     event.preventDefault();
     var newSpace = new Space(0,2, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r0c2").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r1c0").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(1,0, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r1c0").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r1c1").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(1,1, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r1c1").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r1c2").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(1,2, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r1c2").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r2c0").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(2,0, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r2c0").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r2c1").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(2,1, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r2c1").text(currentPlayer.mark);
+    switchPlayer();
+  });
+  $("#r2c2").click(function(event) {
+    event.preventDefault();
+    var newSpace = new Space(2,2, currentPlayer.mark);
+    gameBoard.change(newSpace);
+    $("#out-r2c2").text(currentPlayer.mark);
+    switchPlayer();
   });
 });
 

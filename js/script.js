cbb4079b813c671827bc1661051b699e2c75914b
Esvethlynna Pangelinan, Joel Bakken, Laura Hamilton and Mick Sexton <joel@>
2017-06-15 13:53:14 -0700

    add validate for already populated squares

--- js/script.js
+++ js/script.js
@@ -37,6 +37,11 @@ function switchPlayer() {
   }
 }
 
+function checkSpace(x, y) {
+  if (gameBoard.board[x][y] === "X" || gameBoard.board[x][y] === "O") {
+    return true;
+  } else { return false;}
+}
 
 //UI LOGIC
 
@@ -44,74 +49,119 @@ var gameBoard = new Gameboard();
 var playerX = new Player("X");
 var playerO = new Player("O");
 var currentPlayer = playerO;
-
+var validate = false;
 
 $(document).ready(function() {
   //click listener for row0 col0, when clicked:
   $("#r0c0").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(0,0);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(0,0, currentPlayer.mark);
     gameBoard.change(newSpace);
     //output AddClass X or O or text from currentPlayer.mark within span in li
     $("#out-r0c0").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   //click listener 2...
   $("#r0c1").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(0,1);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(0,1, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r0c1").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r0c2").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(0,2);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(0,2, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r0c2").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r1c0").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(1,0);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(1,0, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r1c0").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r1c1").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(1,1);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(1,1, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r1c1").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r1c2").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(1,2);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(1,2, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r1c2").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r2c0").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(2,0);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(2,0, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r2c0").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r2c1").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(2,1);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(2,1, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r2c1").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
   $("#r2c2").click(function(event) {
     event.preventDefault();
+    validate = checkSpace(2,2);
+    if (validate === true) {
+      alert('Pick another space');
+    } else {
     var newSpace = new Space(2,2, currentPlayer.mark);
     gameBoard.change(newSpace);
     $("#out-r2c2").text(currentPlayer.mark);
     switchPlayer();
+    }
   });
 });
 
